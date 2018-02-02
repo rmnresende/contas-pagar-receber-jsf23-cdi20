1e69a17efb02566f88c7865b7a3730be242cd9f1
Renan Resende <rmnresende@gmail.com>
2018-02-02 13:32:59 -0200

    Configuração inicial + template.

---
+++ src/main/java/br/com/dynamicdev/ConfigurationBean.java
@@ -0,0 +1,18 @@
+package br.com.dynamicdev;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+import static javax.faces.annotation.FacesConfig.Version.JSF_2_3;
+
+/**
+ * Classe necessaria para usar novos recursos da versao 2.3
+ *
+ * @author renan
+ *
+ */
+
+@FacesConfig(version = JSF_2_3)
+@ApplicationScoped
+public class ConfigurationBean {
+
+}
